2bd21f0b8a26effbd8cf0b3b539460325ac79b82
sthornton <sthorn8740@gmail.com>
2015-08-17 13:28:16 -0400

    initial checkin

---
+++ EasyNetQ Test/Publisher/Publisher.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasyNetQ;
+using Messages;
+using System.Threading;
+
+namespace Publisher
+{
+    class Publisher
+    {
+        static void Main(string[] args)
+        {
+            using (var bus = RabbitHutch.CreateBus("host=localhost;username=user1;password=password"))
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    bus.Publish<NewDevice>(new NewDevice { Id = i, Name = "ITEM" + i, DateCreated = DateTime.UtcNow });
+                    Console.WriteLine("sent");
+                    Thread.Sleep(1000);
+                }
+                Console.WriteLine("done");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ EasyNetQ Test/Subscriber/Subscriber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasyNetQ;
+using Messages;
+
+namespace Subscriber
+{
+    class Subscriber
+    {
+        static void Main(string[] args)
+        {
+            using (var bus = RabbitHutch.CreateBus("host=localhost;username=user2;password=password"))
+            {
+                bus.Subscribe<NewDevice>("Devices.New", x => Console.WriteLine("Id: " + x.Id + ", Name: "  + ", Created: " + x.DateCreated));
+                Console.WriteLine("waiting");
+                Console.ReadLine();
+            }
+        }
+    }
+}
